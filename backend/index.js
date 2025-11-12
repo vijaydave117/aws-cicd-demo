aeedf24f4e877984e3f91081a5cb40ae4eacb79e
Vijay Dave <vijaydave117@gmail.com>
2025-11-12 14:41:26 +0530

    Checking CICD pipeline

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("AWS Backend CI/CD Test Successful!");
+  res.send("Working?");
 });
 
 app.get("/", (req, res) => res.status(200).send("OK")); 
