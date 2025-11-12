ff5e2dcc4b6a2c030d5c987930d86eddeb631431
Vijay Dave <vijaydave117@gmail.com>
2025-11-12 15:27:31 +0530

    Backend yaml file changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend CICD pipeline is workng fine now!");
+  res.send("Backend CICD pipeline is workng fine!");
 });
 
 app.get("/", (req, res) => res.status(200).send("OK")); 
