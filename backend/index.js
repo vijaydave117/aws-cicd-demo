214fc2181df5fab9d43cb5a0636cab77f5c8fcd4
Vijay Dave <vijaydave117@gmail.com>
2025-11-13 14:43:08 +0530

    Changed backend file to check CICD is working or not

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Root route — visible in browser
 app.get("/", (req, res) => {
-  res.send("AWS Backend CI/CD pipeline is working Now");
+  res.send("AWS Backend CI/CD pipeline is working Nowwwww");
 });
 
 // Health check endpoints for ECS / ALB
